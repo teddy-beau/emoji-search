a13533174b31d86c6e77a1d943719acf373330ac
teddy-beau <hello@teddy-beau.com>
2021-02-05 11:23:06 +0100

    new update to code

--- src/components/Line.js
+++ src/components/Line.js
@@ -1,13 +1,4 @@
-import data from "../emojiList.json";
-
-const Line = ({ search }) => {
-   const results = [];
-   for (let i = 0; i < data.length; i++) {
-      if (data[i].keywords.match(search)) {
-         results.push(data[i]);
-      }
-   }
-
+const Line = ({ results }) => {
    if (results.length === 0) {
       return <div className="nothing">No emoji found 🙁</div>;
    } else {
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,37 @@
 import { useState } from "react";
 import Line from "./Line";
+import data from "../emojiList.json";
 
 const Search = () => {
    const [search, setSearch] = useState("");
+   // const [search, setSearch] = useState(data.slice(0, 20));
+
+   const results = [];
+   for (let i = 0; i < data.length; i++) {
+      // if (data[i].keywords.indexOf(search) !== -1) { // Alternative
+      if (data[i].keywords.match(search)) {
+         // if (results.length >= 20) {
+         //    break;
+         // } else {
+         // }
+         results.push(data[i]);
+      }
+      // }
+   }
 
    return (
       <>
          <h1>😎 Emoji Search 😎</h1>
-         <form>
-            <input
-               type="search"
-               name="search"
-               placeholder="Which Emoji are you looking for?"
-               value={search}
-               onChange={(event) => {
-                  setSearch(event.target.value);
-               }}
-            />
-            <Line search={search} />
-         </form>
+         <input
+            type="search"
+            name="search"
+            placeholder="Which Emoji are you looking for?"
+            value={search}
+            onChange={(event) => {
+               setSearch(event.target.value);
+            }}
+         />
+         <Line results={results} />
       </>
    );
 };
